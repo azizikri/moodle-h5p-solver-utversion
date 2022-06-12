5e680ee72292b3df589528d0cbaae5a5ea48927a
Zikri Endisyah Munandar <azizikri@gmail.com>
2022-06-13 00:16:23 +0700

    update for univ terbuka moodle

--- getAnswers.js
+++ getAnswers.js
@@ -1,21 +1,16 @@
 let json = H5PIntegration.contents;
-let interactions = JSON.parse(json[Object.keys(json)[0]].jsonContent).interactiveVideo.assets.interactions;
 
+let interactions = JSON.parse(json[Object.keys(json)[0]].jsonContent).questions;
+console.log(interactions);
 let html = "";
 html += `<h1>Answers</h1>`
 
 for (let i = 0; i < interactions.length; i++) {
     interaction = interactions[i]
-    q_type=interaction.libraryTitle
-    params=interaction.action.params
+    params=interaction.params
 
     html += `<h2>Question ${i + 1}</h2>`
 
-    if (q_type=="Single Choice Set") {
-        answer=params.choices[0].answers[0]
-        html += `<p>${answer}</p>`
-        console.log(answer)
-    } else if (q_type=="Multiple Choice") {
         answers = params.answers
         for (let i = 0; i < answers.length; i++) {
             answer = answers[i]
@@ -23,12 +18,6 @@ for (let i = 0; i < interactions.length; i++) {
                 html += `<p>${answer.text}</p>`
                 console.log(answer.text)
         }
-    } else if (q_type=="True/False Question") {
-        console.log(params.correct)
-        html += `<p>${params.correct}</p>`
-    } else 
-        console.log("wait")
-    
     console.log("\n")
 }
 
